5192f18ad5e1e09f77a6bd4a98bffaffe8ccaa6f
Eddie Moya <eddie.moya@gmail.com>
2013-03-18 20:56:53 -0500

    adding option to menu widget for selecting a layout

--- communities-menu-widget.php
+++ communities-menu-widget.php
@@ -214,7 +214,12 @@ class Communities_Menu_Widget extends WP_Widget {
 		$nav_menu = isset( $instance['nav_menu'] ) ? $instance['nav_menu'] : '';
 
 		// Get menus
-		$menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
+		$menu_obj = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
+        $menus = array();
+
+        foreach ( $menu_obj as $menu ) {
+            $menus[$menu->term_id] =$menu->name;
+        }
 
 		// If no menus exists, direct the user to go and create some.
 		if ( !$menus ) {
@@ -222,24 +227,39 @@ class Communities_Menu_Widget extends WP_Widget {
 			return;
 		}
 
-        /* Examples of input fields one at a time. */
-        $this->form_field('title', 'text', 'Title', $instance);
-        $this->form_field('sub-title', 'text', 'Sub Title', $instance);
-        ?>
-        <p>
-			<label for="<?php echo $this->get_field_id('nav_menu'); ?>"><?php _e('Select Menu:'); ?></label>
-			<select id="<?php echo $this->get_field_id('nav_menu'); ?>" name="<?php echo $this->get_field_name('nav_menu'); ?>">
-		<?php
-			foreach ( $menus as $menu ) {
-				$selected = $nav_menu == $menu->term_id ? ' selected="selected"' : '';
-				echo '<option'. $selected .' value="'. $menu->term_id .'">'. $menu->name .'</option>';
-			}
-		?>
-			</select>
-		</p><?php
+        $fields = array(
+            array(
+                'field_id' => 'title',
+                'type' => 'text',
+                'label' => 'Title'
+            ),
+            array(
+                'field_id' => 'sub-title',
+                'type' => 'text',
+                'label' => 'Sub Title'
+            ),
+            array(
+                'field_id' => 'nav_menu',
+                'type' => 'select',
+                'label' => 'Menu',
+                'options' => $menus
+            ),
+            array(
+                'field_id' => 'menu_layout',
+                'type' => 'select',
+                'label' => 'Menu Layout',
+                'options' => array( 
+                    'standard' => 'Standard (default)',
+                    'grid' => 'Grid',
+                    //'subnav' => 'Sub-navigation'
+                )
+            ),
+
+        );
+
+        $this->form_fields($fields, $instance);
     }
     
-
     /**
      * Helper function - does not need to be part of widgets, this is custom, but 
      * is helpful in generating multiple input fields for the admin form at once. 
@@ -254,12 +274,22 @@ class Communities_Menu_Widget extends WP_Widget {
      * @param array $instance   [Required] Current instance of widget option values.
      * @return void
      */
-    private function form_fields($fields, $instance){
-        foreach($fields as &$field){
+    private function form_fields($fields, $instance, $group = false){
+        
+        if($group) {
+            echo "<p>";
+        }
+            
+        foreach($fields as $field){
+            
             extract($field);
             $label = (!isset($label)) ? null : $label;
             $options = (!isset($options)) ? null : $options;
-            $this->form_field($field_id, $type, $label, $instance, $options);
+            $this->form_field($field_id, $type, $label, $instance, $options, $group);
+        }
+        
+        if($group){
+             echo "</p>";
         }
     }
     
@@ -280,20 +310,22 @@ class Communities_Menu_Widget extends WP_Widget {
      * 
      * @return void
      */
-    private function form_field($field_id, $type, $label, $instance, $options = array()){
+    private function form_field($field_id, $type, $label, $instance, $options = array(), $group = false){
   
-        ?><p><?php
+        if(!$group)
+             echo "<p>";
+            
         $input_value = (isset($instance[$field_id])) ? $instance[$field_id] : '';
         switch ($type){
             
             case 'text': ?>
             
                     <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
-                    <input id="<?php echo $this->get_field_id( $field_id ); ?>" style="<?php echo (isset($style)) ? $style : ''; ?>" class="widefat" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
+                    <input type="text" id="<?php echo $this->get_field_id( $field_id ); ?>" class="widefat" style="<?php echo (isset($style)) ? $style : ''; ?>" class="" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
                 <?php break;
             
             case 'select': ?>
-                    
+                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
                     <select id="<?php echo $this->get_field_id( $field_id ); ?>" class="widefat" name="<?php echo $this->get_field_name($field_id); ?>">
                         <?php
                             foreach ( $options as $value => $label ) :  ?>
@@ -306,7 +338,7 @@ class Communities_Menu_Widget extends WP_Widget {
                         ?>
                     </select>
                     
-				<?php break;
+                <?php break;
                 
             case 'textarea':
                 
@@ -326,13 +358,22 @@ class Communities_Menu_Widget extends WP_Widget {
                     
                 <?php break;
             
+ 
+            case 'hidden': ?>
+                    <input id="<?php echo $this->get_field_id( $field_id ); ?>" type="hidden" style="<?php echo (isset($style)) ? $style : ''; ?>" class="widefat" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
+                <?php break;
+ 
+            
             case 'checkbox' : ?>
                     <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id($field_id); ?>" name="<?php echo $this->get_field_name($field_id); ?>"<?php checked( (!empty($instance[$field_id]))); ?> />
-                	<label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?></label>
+                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?></label>
                 <?php
         }
         
-        ?></p><?php
+        if(!$group)
+             echo "</p>";
+            
+       
     }
 }
 
