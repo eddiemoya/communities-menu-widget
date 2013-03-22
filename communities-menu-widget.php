9f247f3fa8d0cfc29b9483aa333ea6134bb75caa
M Day <mday@ushofml299008.kih.kmart.com>
2013-03-22 11:44:31 -0500

    Modified walker to include an image

--- communities-menu-widget.php
+++ communities-menu-widget.php
@@ -107,11 +107,13 @@ class Communities_Menu_Widget extends WP_Widget {
 		}
 
 		echo '<section class="content-body clearfix">';
+        $gol = ($instance['menu_layout'] == "grid") ? "grid" : "list";
 
 		wp_nav_menu( array( 
-			'container_class'      => 'subnav-items', 
+			'container_class'      => 'subnav-items image-' . $gol . '-style', 
 			'fallback_cb' => '', 
-			'menu' => $nav_menu 
+			'menu' => $nav_menu,
+            'walker' => new Walker_Nav_Menu_Image()
 			) );
         
         if(is_object(get_queried_object()) && (isset(get_queried_object()->term_id) || isset(get_queried_object()->term_id) ) ){
@@ -405,6 +407,101 @@ class Communities_Menu_Widget extends WP_Widget {
 		</p>
 		<?php
 	}
+}
+
+class Walker_Nav_Menu_Image extends Walker {
+    /**
+     * @see Walker::$tree_type
+     * @since 3.0.0
+     * @var string
+     */
+    var $tree_type = array( 'post_type', 'taxonomy', 'custom' );
+
+    /**
+     * @see Walker::$db_fields
+     * @since 3.0.0
+     * @todo Decouple this.
+     * @var array
+     */
+    var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
+
+    /**
+     * @see Walker::start_lvl()
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int $depth Depth of page. Used for padding.
+     */
+    function start_lvl(&$output, $depth) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"sub-menu\">\n";
+    }
+
+    /**
+     * @see Walker::end_lvl()
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int $depth Depth of page. Used for padding.
+     */
+    function end_lvl(&$output, $depth) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul>\n";
+    }
+
+    /**
+     * @see Walker::start_el()
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item Menu item data object.
+     * @param int $depth Depth of menu item. Used for padding.
+     * @param int $current_page Menu item ID.
+     * @param object $args
+     */
+    function start_el(&$output, $item, $depth, $args) {
+        global $wp_query;
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        $class_names = $value = '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        $class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+        $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        $output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+        $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= sprintf('<span class="icon"><img src="%s" /></span>', $item->image);
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+
+    /**
+     * @see Walker::end_el()
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item Page data object. Not used.
+     * @param int $depth Depth of page. Not Used.
+     */
+    function end_el(&$output, $item, $depth) {
+        $output .= "</li>\n";
+    }
 }
 
 Communities_Menu_Widget::register_widget();
