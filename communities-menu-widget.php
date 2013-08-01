5c92a5bda360cb0b0789b7e3bc081815aa0b266e
dev <dev@searsdevheadless.searsdevheadless.org>
2013-08-01 13:19:47 -0500

    The walker got hosed somewhere, adding it back in

--- communities-menu-widget.php
+++ communities-menu-widget.php
@@ -91,29 +91,22 @@ class Communities_Menu_Widget extends WP_Widget {
 		$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
         $instance['sub-title'] = apply_filters( 'widget_title', empty( $instance['sub-title'] ) ? '' : $instance['sub-title'], $instance, $this->id_base );
 
+		//print_pre($args);
 		echo $args['before_widget'];
 
 		if ( !empty($instance['title']) )
-		{
-			echo $args['before_title'];
-			echo "<h3>{$instance['title']}</h3>";
-			
-			if(!empty($instance['sub-title']))
-			{
-				echo "<h4>{$instance['sub-title']}</h4>";
-			}
-			
-			echo $args['after_title'];
-		}
+			echo $args['before_title'] . $instance['title'];
+            echo "<h4>".$instance['sub-title']."</h4>";
+            echo $args['after_title'];
+
 
 		echo '<section class="content-body clearfix">';
-        $gol = ($instance['menu_layout'] == "grid") ? "grid" : "list";
 
 		wp_nav_menu( array( 
-			'container_class'      => 'subnav-items image-' . $instance['menu_layout'] . '-style', 
+			'container_class'      => 'subnav-items', 
 			'fallback_cb' => '', 
 			'menu' => $nav_menu,
-            'walker' => new Walker_Nav_Menu_Image()
+			'walker' => new Walker_Nav_Menu_Image()
 			) );
         
         if(is_object(get_queried_object()) && (isset(get_queried_object()->term_id) || isset(get_queried_object()->term_id) ) ){
@@ -216,12 +209,7 @@ class Communities_Menu_Widget extends WP_Widget {
 		$nav_menu = isset( $instance['nav_menu'] ) ? $instance['nav_menu'] : '';
 
 		// Get menus
-		$menu_obj = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
-        $menus = array();
-
-        foreach ( $menu_obj as $menu ) {
-            $menus[$menu->term_id] =$menu->name;
-        }
+		$menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
 
 		// If no menus exists, direct the user to go and create some.
 		if ( !$menus ) {
@@ -229,39 +217,24 @@ class Communities_Menu_Widget extends WP_Widget {
 			return;
 		}
 
-        $fields = array(
-            array(
-                'field_id' => 'title',
-                'type' => 'text',
-                'label' => 'Title'
-            ),
-            array(
-                'field_id' => 'sub-title',
-                'type' => 'text',
-                'label' => 'Sub Title'
-            ),
-            array(
-                'field_id' => 'nav_menu',
-                'type' => 'select',
-                'label' => 'Menu',
-                'options' => $menus
-            ),
-            array(
-                'field_id' => 'menu_layout',
-                'type' => 'select',
-                'label' => 'Menu Layout',
-                'options' => array( 
-                    'list' => 'List (default)',
-                    'grid' => 'Grid',
-                    'subnav' => 'Sub-Navigation'
-                )
-            ),
-
-        );
-
-        $this->form_fields($fields, $instance);
+        /* Examples of input fields one at a time. */
+        $this->form_field('title', 'text', 'Title', $instance);
+        $this->form_field('sub-title', 'text', 'Sub Title', $instance);
+        ?>
+        <p>
+			<label for="<?php echo $this->get_field_id('nav_menu'); ?>"><?php _e('Select Menu:'); ?></label>
+			<select id="<?php echo $this->get_field_id('nav_menu'); ?>" name="<?php echo $this->get_field_name('nav_menu'); ?>">
+		<?php
+			foreach ( $menus as $menu ) {
+				$selected = $nav_menu == $menu->term_id ? ' selected="selected"' : '';
+				echo '<option'. $selected .' value="'. $menu->term_id .'">'. $menu->name .'</option>';
+			}
+		?>
+			</select>
+		</p><?php
     }
     
+
     /**
      * Helper function - does not need to be part of widgets, this is custom, but 
      * is helpful in generating multiple input fields for the admin form at once. 
@@ -276,22 +249,12 @@ class Communities_Menu_Widget extends WP_Widget {
      * @param array $instance   [Required] Current instance of widget option values.
      * @return void
      */
-    private function form_fields($fields, $instance, $group = false){
-        
-        if($group) {
-            echo "<p>";
-        }
-            
-        foreach($fields as $field){
-            
+    private function form_fields($fields, $instance){
+        foreach($fields as &$field){
             extract($field);
             $label = (!isset($label)) ? null : $label;
             $options = (!isset($options)) ? null : $options;
-            $this->form_field($field_id, $type, $label, $instance, $options, $group);
-        }
-        
-        if($group){
-             echo "</p>";
+            $this->form_field($field_id, $type, $label, $instance, $options);
         }
     }
     
@@ -312,22 +275,20 @@ class Communities_Menu_Widget extends WP_Widget {
      * 
      * @return void
      */
-    private function form_field($field_id, $type, $label, $instance, $options = array(), $group = false){
+    private function form_field($field_id, $type, $label, $instance, $options = array()){
   
-        if(!$group)
-             echo "<p>";
-            
+        ?><p><?php
         $input_value = (isset($instance[$field_id])) ? $instance[$field_id] : '';
         switch ($type){
             
             case 'text': ?>
             
                     <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
-                    <input type="text" id="<?php echo $this->get_field_id( $field_id ); ?>" class="widefat" style="<?php echo (isset($style)) ? $style : ''; ?>" class="" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
+                    <input id="<?php echo $this->get_field_id( $field_id ); ?>" style="<?php echo (isset($style)) ? $style : ''; ?>" class="widefat" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
                 <?php break;
             
             case 'select': ?>
-                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
+                    
                     <select id="<?php echo $this->get_field_id( $field_id ); ?>" class="widefat" name="<?php echo $this->get_field_name($field_id); ?>">
                         <?php
                             foreach ( $options as $value => $label ) :  ?>
@@ -340,7 +301,7 @@ class Communities_Menu_Widget extends WP_Widget {
                         ?>
                     </select>
                     
-                <?php break;
+				<?php break;
                 
             case 'textarea':
                 
@@ -360,24 +321,16 @@ class Communities_Menu_Widget extends WP_Widget {
                     
                 <?php break;
             
- 
-            case 'hidden': ?>
-                    <input id="<?php echo $this->get_field_id( $field_id ); ?>" type="hidden" style="<?php echo (isset($style)) ? $style : ''; ?>" class="widefat" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
-                <?php break;
- 
-            
             case 'checkbox' : ?>
                     <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id($field_id); ?>" name="<?php echo $this->get_field_name($field_id); ?>"<?php checked( (!empty($instance[$field_id]))); ?> />
-                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?></label>
+                	<label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?></label>
                 <?php
         }
         
-        if(!$group)
-             echo "</p>";
-            
-       
+        ?></p><?php
     }
 }
+
 
 
 
@@ -449,6 +402,7 @@ class Communities_Menu_Widget extends WP_Widget {
 		<?php
 	}
 }
+
 
 class Walker_Nav_Menu_Image extends Walker {
     /**
